ab7fc7c32ada7191b15d8138847ade7066b1cc6e
HeineTL <116063325+HeineTL@users.noreply.github.com>
2022-11-04 12:52:07 +0100

    Orakel ferdig

--- Filer/Orakel/script.js
+++ Filer/Orakel/script.js
@@ -1,42 +1,13 @@
-let answer;
+let orakelSvar = ['Ja', 'Nei', 'Kanskje'];
 
-let answerText = document.getElementById("answerText");
-let answerHeadline = document.getElementById("answerHeadline");
+function askQuestion() {
+    document.getElementById("answerHeadline").innerHTML = "ORAKELET SIER:";
 
-function upperCase() {
-    answer = document.getElementById("string").value;
-
-    let response = answer.toUpperCase();
-
-    respond(response);
-}
-function lowerCase() {
-    answer = document.getElementById("string").value;
-
-    let response = answer.toLowerCase();
-
-    respond(response);
-}
-function firstLetterToUppercase() {
-    answer = "" + document.getElementById("string").value;
-
-    let response = answer.charAt(0).toUpperCase() + answer.slice(1);
-
-    respond(response);
-}
-function reverseString() {
-    answer = document.getElementById("string").value;
-
-    let responseSplit = answer.split("");
-    
-    let responseReverseArray = responseSplit.reverse();
-
-    let response = responseReverseArray.join("");
-
-    respond(response);
+    document.getElementById("answerText").innerHTML = randomAnswer();
 }
 
-function respond(responseAnswer) {
-    answerHeadline.innerHTML = "Svar:";
-    answerText.innerHTML = responseAnswer;
+function randomAnswer () {
+    let randomNummer = Math.floor(Math.random() * orakelSvar.length);
+
+    return orakelSvar[randomNummer];
 }
